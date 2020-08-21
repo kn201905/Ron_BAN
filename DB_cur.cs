31a729c8361578caabda2b0016aeac699062dade
kn201905 <zz2018-dr@yahoo.co.jp>
2020-08-21 17:01:20 +0900

    Update DB_cur.cs

--- DB_cur.cs
+++ DB_cur.cs
@@ -23,7 +23,7 @@ namespace Ron_BAN
 
 	ref struct JSON_Reader
 	{
-		Utf8JsonReader ms_reader;
+		Utf8JsonReader m_reader;
 
 		public JSON_Reader(byte[] buf_utf8)
 		{
@@ -32,37 +32,59 @@ namespace Ron_BAN
 				AllowTrailingCommas = true,
 				CommentHandling = JsonCommentHandling.Skip
 			};
-			ms_reader = new Utf8JsonReader(buf_utf8, options);
+			m_reader = new Utf8JsonReader(buf_utf8, options);
 		}
 
+		// -----------------------------------------
 		public JsonTokenType GetNextType()
 		{
-			if (ms_reader.Read() == false)
+			if (m_reader.Read() == false)
 			{ throw new Exception("!!! JSON のパースに失敗しました。"); }
 
-			return ms_reader.TokenType;
+			return m_reader.TokenType;
 		}
 
+		// -----------------------------------------
 		public string GetString()
 		{
-			return ms_reader.GetString();
+			return m_reader.GetString();
 		}
 	
+		// -----------------------------------------
 		public void Search_users_key()
 		{
 			while (true)
 			{
-				if (ms_reader.Read() == false)
+				if (m_reader.Read() == false)
 				{ throw new Exception("!!! JSON のパースに失敗しました。"); }
 
-				if (ms_reader.TokenType != JsonTokenType.PropertyName) { continue; }
-				if (ms_reader.GetString() == "users") { return; }
+				if (m_reader.TokenType != JsonTokenType.PropertyName) { continue; }
+				if (m_reader.GetString() == "users") { return; }
+			}
+		}
+
+		// -----------------------------------------
+		public void Search_talks_key()
+		{
+			while (true)
+			{
+				if (m_reader.Read() == false)
+				{ throw new Exception("!!! JSON のパースに失敗しました。"); }
+
+				if (m_reader.TokenType != JsonTokenType.PropertyName) { continue; }
+				if (m_reader.GetString() == "talks")
+				{
+					if (this.GetNextType() != JsonTokenType.StartArray)
+					{ throw new Exception("!!! JSON のパースに失敗しました。"); }
+
+					return;
+				}
 			}
 		}
 	}
 
 	///////////////////////////////////////////////////////////////////////////////////////
-	// 現時点で部屋にいるユーザを管理するクラス（BAN に利用）
+	// 現時点で部屋にいるユーザを管理するクラス
 
 	class UInfo
 	{
@@ -80,7 +102,7 @@ namespace Ron_BAN
 		public bool mb_multi;
 	}
 
-	///////////////////////////////////////////////////////////////////////////////////////
+	// ------------------------------------------------------------------------------------
 
 	static class UInfo_onRoom
 	{
@@ -154,12 +176,65 @@ namespace Ron_BAN
 	}
 
 	///////////////////////////////////////////////////////////////////////////////////////
+	// on talks の退室者情報を保持するクラス（利用目的：退室者の BAN 指定）
+
+	static class UInfo_Exited
+	{
+		enum EUStt : byte { EN_OnTalks = 1, EN_Banned = 2 }
+
+		static List<string> msa_encip_on_talks = new List<string>();
+		static List<List<string>> msa_unames_on_talks = new List<List<string>>();  // talk 上の unames
+		static List<EUStt> msa_flags = new List<EUStt>();
+
+		public static void Clear_OnTalksFlag()
+		{
+			for (int idx = msa_flags.Count; --idx >= 0; ) { msa_flags[idx] &= ~EUStt.EN_OnTalks; }
+		}
+
+		public static void Rgst_EUser_on_talks(string encip, string uname, List<string> uname_s, StringBuilder ret_sb)
+		{
+			int eidx = msa_encip_on_talks.IndexOf(encip);
+			if (eidx < 0)
+			{
+				msa_encip_on_talks.Add(encip);
+				msa_unames_on_talks.Add(DB_cur.NewStringList(uname));
+				msa_flags.Add(EUStt.EN_OnTalks);
+				ret_sb.Append($"・退室者 [{string.Join(", ", uname_s)}]\r\n{encip}\r\n\r\n");
+			}
+			else
+			{
+				int name_idx = msa_unames_on_talks[eidx].IndexOf(uname);
+				if (name_idx < 0)
+				{
+					msa_unames_on_talks[eidx].Add(uname);
+					ret_sb.Append($"・退室者 [{string.Join(", ", uname_s)}]\r\n{encip}\r\n\r\n");
+				}
+				msa_flags[eidx] |= EUStt.EN_OnTalks;
+			}
+		}
+
+		public static void Remove_EUser_disappear()
+		{
+			// idx による削除は後方から行う必要がある
+			for (int idx = msa_flags.Count; --idx >= 0; )
+			{
+				if ((msa_flags[idx] & EUStt.EN_OnTalks) == 0)
+				{
+					msa_encip_on_talks.RemoveAt(idx);
+					msa_unames_on_talks.RemoveAt(idx);
+					msa_flags.RemoveAt(idx);
+				}
+			}
+		}
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////
 	// 現セッションの部屋情報を管理するクラス
 
 	static class DB_cur
 	{
 		static StringBuilder ms_ret_sb = new StringBuilder(256);  // 初期値は後ほど調整、、、
-		enum UStt : uint { EN_ON = 1, EN_OFF = 2, EN_STAY = 3, EN_ReENT = 4, EN_NEW = 5, EN_Mask_InOut = 0xf
+		enum UStt : byte { EN_ON = 1, EN_OFF = 2, EN_STAY = 3, EN_ReENT = 4, EN_NEW = 5, EN_Mask_InOut = 0xf
 								, EN_MULTI = 16, EN_MULTI_Warned = 32 }
 
 		static List<string> msa_eip = new List<string>();
@@ -252,7 +327,7 @@ namespace Ron_BAN
 					if ((ustt_old & UStt.EN_MULTI_Warned) == 0)
 					{
 						b_notify = true;
-						ms_ret_sb.Append("・多窓ユーザ");
+						ms_ret_sb.Append("--- 多窓ユーザ ---");
 						msa_ustt[idx] |= UStt.EN_MULTI_Warned;
 					}
 					msa_ustt[idx] = UStt.EN_ON | UStt.EN_MULTI | UStt.EN_MULTI_Warned;
@@ -268,6 +343,22 @@ namespace Ron_BAN
 
 			// ------------------------------------------------------------
 			// talks の解析
+			json_reader.Search_talks_key();  // JsonTokenType.StartArray の位置に設定される
+
+			UInfo_Exited.Clear_OnTalksFlag();  // Remove_EUser_disappear() のための準備
+
+			while (true)
+			{
+				JsonTokenType next_type = json_reader.GetNextType();
+				if (next_type == JsonTokenType.EndArray) { break; }
+
+				if (next_type != JsonTokenType.StartObject)
+				{ throw new Exception("!!! JSON のパースに失敗しました。"); }
+
+				Chk_NextTalk(ref json_reader);
+			}
+
+			UInfo_Exited.Remove_EUser_disappear();
 
 			return ms_ret_sb;
 		}
@@ -318,7 +409,7 @@ namespace Ron_BAN
 			{
 				// 新規登録の eip
 				msa_eip.Add(uencip.m_str_encip);
-				msa_unames.Add(NewListItem(str_uname));
+				msa_unames.Add(NewStringList(str_uname));
 				msa_ustt.Add(UStt.EN_NEW);
 			}
 			else
@@ -352,11 +443,66 @@ namespace Ron_BAN
 			UInfo_onRoom.Attend(ref uid, str_uname, ref uencip);
 		}
 
-		static List<string> NewListItem(string str)
+		public static List<string> NewStringList(string str)
 		{
 			var ret_obj = new List<string>();
 			ret_obj.Add(str);
 			return ret_obj;
+		}
+
+		// ------------------------------------------------------------------------------------
+		// ポジションを JsonTokenType.StartObject にした状態でコールすること
+		// JsonTokenType.EndObject のポジションでリターンされる
+		static void Chk_NextTalk(ref JSON_Reader json_reader)
+		{
+			string str_eip = null;
+			string str_uname = null;
+			bool b_type_exit = false;
+
+			while (true)
+			{
+				JsonTokenType next_type = json_reader.GetNextType();
+				if (next_type == JsonTokenType.PropertyName)
+				{
+					// key の取得
+					string str_key = json_reader.GetString();
+
+					// value の読み込み
+					if (json_reader.GetNextType() != JsonTokenType.String) { continue; }
+					string str_value = json_reader.GetString();
+
+					if (str_key == "encip") { str_eip = str_value; }
+					else if (str_key == "name") { str_uname = str_value; }
+					else if (str_key == "type" && str_value == "exit") { b_type_exit = true; }
+
+					continue;
+				}
+				else if (next_type == JsonTokenType.EndObject) { break; }
+
+				throw new Exception("!!! JSON のパースに失敗しました。");
+			}
+
+			if (b_type_exit == false) { return; }
+
+			if (str_eip == null || str_uname == null)
+			{ throw new Exception("!!! JSON のパースに失敗しました。"); }
+
+			int eidx = msa_eip.IndexOf(str_eip);
+			List<string> unames = null;
+			if (eidx < 0)
+			{
+				msa_eip.Add(str_eip);
+				unames = NewStringList(str_uname);
+				msa_unames.Add(unames);
+				msa_ustt.Add(UStt.EN_OFF);
+			}
+			else
+			{
+				unames = msa_unames[eidx];
+				if (unames.IndexOf(str_uname) < 0) { unames.Add(str_uname); }
+			}
+
+			UInfo_Exited.Rgst_EUser_on_talks(str_eip, str_uname, unames, ms_ret_sb);
 		}
 	}
 }
