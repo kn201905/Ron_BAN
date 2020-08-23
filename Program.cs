72958d637662397905608e43fc93e5fec43a1580
kn201905 <zz2018-dr@yahoo.co.jp>
2020-08-23 10:37:19 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,22 +6,12 @@ namespace Ron_BAN
 {
 	static class Program
 	{
-		static TextBox ms_tbox_status = null;
-
 		[STAThread]
 		static void Main()
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-
-			MainForm main_form = new MainForm();
-			ms_tbox_status = main_form.m_tbox_status;
-			Application.Run(main_form);
-		}
-
-		public static void WriteStatus(string str)
-		{
-			ms_tbox_status.AppendText(str);
+			Application.Run(new MainForm());
 		}
 
 		// ------------------------------------------------
@@ -30,7 +20,6 @@ namespace Ron_BAN
 		{
 			WebProxy web_proxy = new WebProxy("http://***");
 			web_proxy.Credentials = new NetworkCredential("***", "***");
-
 			return web_proxy;
 		}
 
