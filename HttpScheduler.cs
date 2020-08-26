07e934f76e7b2c623e428aa0fa0f066cca578e4e
kn201905 <zz2018-dr@yahoo.co.jp>
2020-08-26 13:28:19 +0900

    Update HttpScheduler.cs

--- HttpScheduler.cs
+++ HttpScheduler.cs
@@ -1,15 +1,22 @@
 namespace Ron_BAN
 {
-		// ------------------------------------------------------------------------------------
 		// テスト１
 		static uint ms_lo_task_id = 1;
 		async void m_btn_test_1_Click(object sender, EventArgs e)
 		{
-			LoTask lo_task = new LoTask() { m_task_id = ms_lo_task_id };
-			ms_lo_task_id++;
-			await HttpScheduler.Set(lo_task);
 
-			ms_RBox_usrMsg.AppendText($"result: {lo_task.m_result}\r\n");
+			tst_lo_task lo_task = new tst_lo_task() { m_task_id = ms_lo_task_id };
+			ms_lo_task_id++;
+
+			try
+			{
+				await tst_scheduler.Set(lo_task);;
+				ms_RBox_usrMsg.AppendText($"result: {lo_task.m_result}\r\n");
+			}
+			catch (Exception ex)
+			{
+				ms_RBox_usrMsg.AppendText(ex.ToString() + "\r\n");
+			}
 		}
 
 		// ------------------------------------------------------------------------------------
@@ -17,19 +24,25 @@ namespace Ron_BAN
 		static uint ms_mid_task_id = 101;
 		async void m_btn_test_2_Click(object sender, EventArgs e)
 		{
-			MidTask mid_task = new MidTask() { m_task_id = ms_mid_task_id };
+			tst_mid_task mid_task = new tst_mid_task() { m_task_id = ms_mid_task_id };
 			ms_mid_task_id++;
 
-			await HttpScheduler.Set(mid_task);
-
-			ms_RBox_usrMsg.AppendText($"result: {mid_task.m_result}\r\n");
+			try
+			{
+				await tst_scheduler.Set(mid_task);;
+				ms_RBox_usrMsg.AppendText($"result: {mid_task.m_result}\r\n");
+			}
+			catch (Exception ex)
+			{
+				ms_RBox_usrMsg.AppendText(ex.ToString() + "\r\n");
+			}
 		}
 
 		// ------------------------------------------------------------------------------------
 
-		static class HttpScheduler
+		static class tst_scheduler
 		{
-			public static Task Set(HttpTask http_task)
+			public static Task Set(tst_http_task http_task)
 			{
 				Task task = http_task.Queueing();
 				http_task.SetLatestTask(task);
@@ -37,7 +50,7 @@ namespace Ron_BAN
 			}
 		}
 
-		public abstract class HttpTask
+		public abstract class tst_http_task
 		{
 			public string m_str_cancel = null;  // これが null でない場合、タスクがキャンセルされたことを表す
 			public uint m_task_id;
@@ -47,12 +60,18 @@ namespace Ron_BAN
 			public abstract void SetLatestTask(Task task);
 		}
 
-		public class MidTask : HttpTask
+		public class tst_mid_task : tst_http_task
 		{
 			public static uint ms_num_mid_task = 0;
 			public static Task ms_mid_task_latest = null;
 
 			public override void SetLatestTask(Task task) { ms_mid_task_latest = task; }
+			public static void ResetQueue()
+			{
+				ms_num_mid_task = 0;
+				ms_mid_task_latest = null;
+			}
+
 			public override async Task Queueing()
 			{
 				ms_num_mid_task++;
@@ -60,9 +79,19 @@ namespace Ron_BAN
 				{
 					await ms_mid_task_latest;
 				}
-				if (LoTask.ms_lo_task_cur != null)
+				if (tst_lo_task.ms_lo_task_cur != null)
 				{
-					await LoTask.ms_lo_task_cur;
+					await tst_lo_task.ms_lo_task_cur;
+				}
+
+				if (m_task_id == 103)
+				{
+					ms_RBox_usrMsg.AppendText("+++ throw テスト\r\n");
+
+					ResetQueue();
+					tst_lo_task.ResetQueue();
+
+					throw new Exception("!!! throw テスト： m_task_id == 103)");
 				}
 
 				ms_RBox_usrMsg.SelectionColor = System.Drawing.Color.FromArgb(255, 100, 50);
@@ -80,13 +109,20 @@ namespace Ron_BAN
 			}
 		}
 
-		public class LoTask : HttpTask
+		public class tst_lo_task : tst_http_task
 		{
 			static uint ms_num_lo_task = 0;
 			static Task ms_lo_task_latest = null;
 			public static Task ms_lo_task_cur = null;
 
 			public override void SetLatestTask(Task task) { ms_lo_task_latest = task; }
+			public static void ResetQueue()
+			{
+				ms_num_lo_task = 0;
+				ms_lo_task_latest = null;
+				// ms_lo_task_cur = null;
+			}
+
 			public override async Task Queueing()
 			{
 				ms_num_lo_task++;
@@ -94,9 +130,9 @@ namespace Ron_BAN
 				{
 					await ms_lo_task_latest;
 				}
-				while (MidTask.ms_num_mid_task > 0)
+				while (tst_mid_task.ms_num_mid_task > 0)
 				{
-					await MidTask.ms_mid_task_latest;
+					await tst_mid_task.ms_mid_task_latest;
 				}
 
 				ms_RBox_usrMsg.AppendText($"--- Lo_Ex_Task　task_id: {m_task_id} <-- START\r\n");
@@ -109,5 +145,5 @@ namespace Ron_BAN
 				ms_RBox_usrMsg.AppendText($"--- Lo_Ex_Task　task_id: {m_task_id} <-- END\r\n");
 				ms_num_lo_task--;
 			}
-		}		
+		}
 }
